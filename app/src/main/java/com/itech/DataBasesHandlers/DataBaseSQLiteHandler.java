aa5b17751c7fdf77b66e9ca8eeb896b48cf9756a
AhmedLattali <ba_lattali@esi.dz>
2015-09-18 21:29:36 +0100

    sqlite v 1.5

--- app/src/main/java/com/itech/DataBasesHandlers/DataBaseSQLiteHandler.java
+++ app/src/main/java/com/itech/DataBasesHandlers/DataBaseSQLiteHandler.java
@@ -17,7 +17,7 @@ import java.util.Arrays;
 
 public class DataBaseSQLiteHandler extends SQLiteOpenHelper {
 
-    private static final int DATABASE_VERSION = 8;
+    private static final int DATABASE_VERSION = 9;
     private static final String DATABASE_NAME = "bemyappdb";
     Context context ;
     private ArrayList<BabyName> babyNames_tous;
